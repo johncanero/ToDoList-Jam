8a39ad27419b34504152bb1be7ec18f201dd9e26
John Canero <johncanero.official@gmail.com>
2022-07-10 08:50:37 +0800

    Algorithmic Code: Get and Post Function

--- app.js
+++ app.js
@@ -9,15 +9,26 @@ const app = express();
 const items = ["Study Web Development", "Build Something"];
 const workItems = [];
 
-// set-up view: EJS
+// SET - UP view: EJS
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 // public > css (styles)
 app.use(express.static("public")); 
 
+// GET function for home route
 
 
 
+// POST function for home route
+
+
+// GET function for work route
+
+
+// POST function for work route
+
+
+// GET function for about route
 
 
 // essentials: running server
